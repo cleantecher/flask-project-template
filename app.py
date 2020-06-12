cb5a7974775d6f11ec3ce077c2ecaa1ac1190d7d
cleantecher <50503689+cleantecher@users.noreply.github.com>
2020-06-11 20:56:22 -0600

    initial demo flow

--- app.py
+++ app.py
@@ -8,58 +8,62 @@ Bootstrap(app)
 def hello():
     return render_template("index.html") 
 
+@app.route("/status")
+def status():
+    return render_template ("status.html")
+
+@app.route("/paysetup")
+def paysetup():
+    return render_template ("paysetup.html")
+
+@app.route("/switchsettings")
+def switchsettings():
+    return render_template ("switchsettings.html")
+
+@app.route("/usersettings")
+def usersettings():
+    return render_template ("usersettings.html")
+
 @app.route("/signup")
-def about():
+def signup():
     return render_template ("signup.html")
 
 @app.route("/signin")
-def about():
+def signin():
     return render_template ("signin.html")
 
 @app.route("/fbsignin")
-def about():
+def fbsignin():
     return render_template ("fbsignin.html")
 
 @app.route("/googlesignin")
-def about():
+def googlesignin():
     return render_template ("googlesingin.html")
 
-@app.route("/UserHomeInfo")
-def about():
-    return render_template ("UserHomeInfo.html")
+@app.route("/pairswitch")
+def pairswitch():
+    return render_template ("pairswitch.html")
 
-@app.route("/paysetup")
-def about():
-    return render_template ("paysetup.html")
-
-@app.route("/pair-switch")
-def about():
-    return render_template ("pair-switch.html")
-
-@app.route("/switchsettings")
-def about():
-    return render_template ("switchsettings.html")
-
-@app.route("/mainstatus")
-def about():
-    return render_template ("mainstatus.html")
+@app.route("/firstlogin")
+def firstlogin():
+    return render_template ("firstlogin.html")
 
 @app.route("/alertscreen")
-def about():
+def alertscreen():
     return render_template ("alertscreen.html")
 
 @app.route("/confirmbuy")
-def about():
+def confirmbuy():
     return render_template ("confirmbuy.html")
 
 @app.route("/confirmsell")
-def about():
+def confirmsell():
     return render_template ("confirmsell.html")
 
 @app.route("/buyingstatus")
-def about():
+def buyingstatus():
     return render_template ("buyingstatus.html")
 
 @app.route("/sellingstatus")
-def about():
+def sellingstatus():
     return render_template ("sellingstatus.html")
--- static/scripts.js
+++ static/scripts.js
@@ -2,3 +2,9 @@
 $("button").click(function(){  
      $(".roundbutton").toggleClass("searching");  
 });  
+
+$('#submit').click(function(){
+     setTimeout(function(){
+          window.location.href='/switchsettings';
+     },3000);
+});
